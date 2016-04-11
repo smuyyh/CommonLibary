cd9d4e23e45db46f8eac3020f0b4b27f36dd941e
smuyyh <352091626@qq.com>
2016-04-11 16:13:16 +0800

    SwipeBackActivity、PullToRefreshActivity

--- app/src/main/java/com/yuyh/common/MainActivity.java
+++ app/src/main/java/com/yuyh/common/MainActivity.java
@@ -1,7 +1,9 @@
 package com.yuyh.common;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pullRefresh(View view){
+        startActivity(new Intent(this, PullToRefreshActivity.class));
+    }
+
+    public void swipe(View view){
+        startActivity(new Intent(this, SwipeBackActivity.class));
     }
 }
